70aced8d5c2783c79d709787fb764dac4fe4f03e
Eugene Staroselets <43090127+starrrets@users.noreply.github.com>
2021-01-07 13:49:54 +0300

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('localhost', 'root', '', '4coder-beta');
+$link = mysqli_connect('localhost', 'root', '', 'coderley');
 mysqli_set_charset($link, 'utf8');
 if (mysqli_connect_errno()) {
     echo 'Ошибка в подключении в бд';
